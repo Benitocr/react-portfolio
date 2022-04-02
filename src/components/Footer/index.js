e0f51f36f1163c47a18dbe15ade5feb0fd05e9a5
Benito <benitocavazosr@yahoo.com>
2022-04-02 18:21:37 -0500

    adding header

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className='flex-row container'>
+    <div className='flex-row container navlist2'>
         <a href = "https://github.com/Benitocr" target="_blank" >
             <img src={require(`../../assets/git.jpg`)}
                 alt="Git Hub Logo"
@@ -17,6 +17,14 @@ function Footer() {
                 alt="LinkedIn logo"
                 className='img-thumbnail mx-1'
                 key = "LinkedIn"
+                style={{width:42, height:42}}
+            />
+        </a>
+        <a href="mailto:benocr78@gmail.com">
+            <img src={require(`../../assets/email.png`)}
+                alt="email logo"
+                className='img-thumbnail mx-1'
+                key = "emaillogo"
                 style={{width:42, height:42}}
             />
         </a>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,29 +3,26 @@ import React from 'react';
 function Nav() {
 
   return (
-    <header className="flex-row px-1">
-        <nav>
-            <ul className="flex-row">
-            <li id = "Name" className='mx-2'>
-                <a href='/'>Benito Cavazos</a>
-            </li>
-            <li className="mx-2">
-                <a href="#about">
-                About me
-                </a>
-            </li>
-            <li className="mx-2">
-                <span>Portfolio</span>
-            </li>
-            <li className="mx-2">
-                <span>Contact</span>
-            </li>
-            <li className="mx-2">
-                <span>Resume</span>
-            </li>
+      <nav className="flex-row navlist">
+            <h1 className="flex-row">Benito Cavazos</h1>
+            <ul className="flex-row " >   
+                <li className="mx-2 ">
+                  About me  
+                </li>
+                <li className="mx-2">
+                    <span>Portfolio</span>
+                </li>
+                <li className="mx-2">
+                    <span>Contact</span>
+                </li>
+                <li className="mx-2">
+                    <span>Resume</span>
+                </li>
             </ul>
-        </nav>
-    </header>
+               
+        </nav>            
+        
+   
   );
 }
 
